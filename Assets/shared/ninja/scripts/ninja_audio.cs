4ae8ba95d776c3fae2c5d6052ca6e05c5744a9ba
Git-Math <matoo3@hotmail.fr>
2019-04-17 19:18:19 +0200

    isolate dash sound

--- Assets/shared/ninja/scripts/ninja_audio.cs
+++ Assets/shared/ninja/scripts/ninja_audio.cs
@@ -13,14 +13,14 @@ public class ninja_audio : MonoBehaviour {
     public AudioClip audio_tp_reappear;
     public float audio_step_length;
     private float time;
-    private AudioSource audio_source;
+    private AudioSource[] audio_source;
     private System.Random rand;
     private bool is_walking;
 
     // Use this for initialization
     void Start () {
         time = 0.0f;
-        audio_source = this.GetComponent<AudioSource>();
+        audio_source = this.GetComponents<AudioSource>();
         rand = new System.Random();
         is_walking = false;
     }
@@ -42,36 +42,36 @@ public class ninja_audio : MonoBehaviour {
     public void dash()
     {
         is_walking = false;
-        audio_source.clip = audio_dash_array[rand.Next(0, audio_dash_array.Length)];
-        audio_source.Play();
+        audio_source[1].clip = audio_dash_array[rand.Next(0, audio_dash_array.Length)];
+        audio_source[1].Play();
     }
 
     public void jump()
     {
         is_walking = false;
-        audio_source.clip = audio_jump_array[rand.Next(0, audio_jump_array.Length)];
-        audio_source.Play();
+        audio_source[0].clip = audio_jump_array[rand.Next(0, audio_jump_array.Length)];
+        audio_source[0].Play();
     }
 
     public void attack_weak()
     {
         is_walking = false;
-        audio_source.clip = audio_attack_weak_array[rand.Next(0, audio_attack_weak_array.Length)];
-        audio_source.Play();
+        audio_source[0].clip = audio_attack_weak_array[rand.Next(0, audio_attack_weak_array.Length)];
+        audio_source[0].Play();
     }
 
     public void tp_cast()
     {
         is_walking = false;
-        audio_source.clip = audio_tp_cast;
-        audio_source.Play();
+        audio_source[0].clip = audio_tp_cast;
+        audio_source[0].Play();
     }
 
     public void tp_reappear()
     {
         is_walking = false;
-        audio_source.clip = audio_tp_reappear;
-        audio_source.Play();
+        audio_source[0].clip = audio_tp_reappear;
+        audio_source[0].Play();
     }
 
     // Update is called once per frame
@@ -84,8 +84,8 @@ public class ninja_audio : MonoBehaviour {
             if (time >= audio_step_length)
             {
                 time = 0.0f;
-                audio_source.clip = audio_step_array[rand.Next(0, audio_step_array.Length)];
-                audio_source.Play();
+                audio_source[0].clip = audio_step_array[rand.Next(0, audio_step_array.Length)];
+                audio_source[0].Play();
             }
         }
     }
